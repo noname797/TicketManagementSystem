9edec160e44a3eb329c43f2822e3fd2dea8c680a
noname797 <sarvesh797@gmail.com>
2022-09-20 16:56:25 +0530

    Login page fixes

--- TicketManagementSystem/Login/urls.py
+++ TicketManagementSystem/Login/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from Login import views
 
-
+app_name = "login"
 urlpatterns=[
-    path("/",views.login,'login'),
+    path('',views.login)
 ]
--- TicketManagementSystem/Login/views.py
+++ TicketManagementSystem/Login/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def login(request):
-    return render('/')
+    return render(request,'index.html')

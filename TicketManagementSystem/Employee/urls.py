4a5009759475c31653ffa63b2844a337c17e487a
noname797 <sarvesh797@gmail.com>
2022-09-21 16:06:14 +0530

    API init

--- TicketManagementSystem/Employee/urls.py
+++ TicketManagementSystem/Employee/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from Employee import views
+from Employee.views import user_history
 
 app_name = "employee"
 urlpatterns=[
     path('raiseReq/',views.raiseReq,name='raiseReq'),
     path('reset',views.reset, name="reset"),
-    path('history',views.user_history,name ='history'),
+    path('history/',views.user_history.as_view(),name ='history'),
     # path('signup',views.signup),
 ]
--- TicketManagementSystem/Employee/views.py
+++ TicketManagementSystem/Employee/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render,HttpResponse,redirect
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from Tickets.models import Ticket
 
 # Create your views here.
 
@@ -8,5 +12,17 @@ def raiseReq(request):
 def reset(request):
     return redirect("/")
 
-def user_history(request):
-    return render(request,"history.html")
+class user_history(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'history.html'
+
+
+    def get(self, request):
+        id=1
+        #print(Ticket.objects.all())
+        queryset = Ticket.objects.all().filter(user_id=id)
+        
+        #queryset = Ticket.objects.all()
+        print(queryset)
+        #return Response()
+        return render(request,"history.html",{'list': queryset, 'id':id})

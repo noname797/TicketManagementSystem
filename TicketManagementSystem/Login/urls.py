b3dfd1e9f9acb321de65b59c4c077a45b85670c4
manindersingh120996 <manindersingh120996@gmail.com>
2022-09-21 17:43:42 +0530

    Signup without API

--- TicketManagementSystem/Login/urls.py
+++ TicketManagementSystem/Login/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from Login import views
+from Login.views import signup
 
 app_name = "login"
 urlpatterns=[
     path('',views.login),
-    path('signup',views.signup),
+    # path('login',views.login),
+    path('signup',views.signup, name='signup'),
     path('reset',views.reset,name="reset")
 ]
--- TicketManagementSystem/Login/views.py
+++ TicketManagementSystem/Login/views.py
@@ -1,5 +1,9 @@
 
+from multiprocessing import context
 from django.shortcuts import render, HttpResponse,redirect
+from Login.models import Profile
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -8,7 +12,36 @@ def login(request):
     return render(request,'index.html')
 
 def signup(request):
-    return render(request,"signup.html")
+    if request.method == 'POST':
+        psno = request.POST.get('psno')
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+
+        password = request.POST.get('password')
+        # confirmpassword = request.POST.get('confirmpassword')
+
+        print(request.POST.get('psno'))
+
+        user_obj = Profile.objects.filter(ps_number = psno)
+
+        if user_obj.exists():
+            mes ='User with this PS Number already Exists.'
+            context ={
+                'error' : mes,
+            }
+            return render(request,'signup.html',context)
+        
+        try:
+            user_obj = Profile.objects.create(ps_number=psno,name=name,email=email,password=password)
+            print(user_obj)
+            user_obj.save()
+            messages.success(request,'Account has been created successfully. You can now Login into TMS.')
+            return redirect('/')
+        except Exception as e:
+            print(e)
+            
+            messages.warning(request, 'There is some problem with the account registeration.Please contact the admin.')
+    return render(request,'signup.html',)
 
 def reset(request):
     return redirect("/")

0b23fdaa112300b59df9adb4357a68cdbf2620aa
noname797 <sarvesh797@gmail.com>
2022-09-21 14:24:43 +0530

    DB changes

--- TicketManagementSystem/Login/models.py
+++ TicketManagementSystem/Login/models.py
@@ -5,7 +5,6 @@ import uuid
 # Create your models here.
 
 class Profile(models.Model):
-    id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     ps_number = models.IntegerField( unique=True)
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
